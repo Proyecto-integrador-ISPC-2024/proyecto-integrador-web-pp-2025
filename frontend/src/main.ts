f25ff89f7275d11d8499408b377e27120388f504
Alvarez Jesica <alvarezjs1990@gmail.com>
2025-05-12 20:07:36 -0300

    Se implementa el momdo Dark (oscuro) pero como servicio

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,11 @@
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark-theme');
+} else {
+  document.body.classList.remove('dark-theme');
+}
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
